66548bedfb592afef3d71afc60c8f861fa71379e
cris <cgoebner@gmail.com>
2024-11-30 12:33:08 -0800

    Got the admin site up and everything is working.

--- equipment_manager/admin.py
+++ equipment_manager/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from equipment_manager.models import EquipmentType, Period, EquipmentManufacturer, EquipmentModel, Instance, RecordType, \
+    RecordDetail, RecordLog
+
+
+@admin.register(EquipmentType)
+class EquipmentTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EquipmentManufacturer)
+class EquipmentManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(EquipmentModel)
+class EquipmentModelAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Instance)
+class InstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecordType)
+class RecordTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecordDetail)
+class RecordDetailAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecordLog)
+class RecordLogAdmin(admin.ModelAdmin):
+    pass
+
+
--- equipment_manager/models.py
+++ equipment_manager/models.py
@@ -1,42 +1,63 @@
-from django.db import models
-from datetime import date
 import uuid
+from datetime import date
 
-from django.db.models import PROTECT
+from django.db import models
+
+
+class EquipmentType(models.Model):
+    type = models.CharField(max_length=100, null=False, help_text='Enter the type of the equipment. (e.g., oven, '
+                                                                  'tape measure etc.)')
+
+    def __str__(self):
+        return self.type
 
 
 class Period(models.Model):
-    period = models.CharField(max_length=50, help_text="Enter periods i.e. monthly, weekly, yearly etc.")
-    days = models.IntegerField(
-        help_text="Enter associated day length of the period. i.e. weeks=7 days, months=30 days, years=365 days etc.")
+    """
+    This table is to ensure that periods are consistent with the relativedelta function used to
+    calculate maintenance and calibration periods.
+    """
+    duration = models.IntegerField(null=False, help_text='Enter the integer value associated with the '
+                                                         'period.')
+    period = models.CharField(max_length=50, null=False, help_text='Enter period. (e.g.,years, months, weeks, '
+                                                                   'days etc.)')
+
+
+    def __str__(self):
+        return f"{self.duration} {self.period}"
 
 
 class EquipmentManufacturer(models.Model):
-    manufacturer = models.CharField(max_length=50)
+    """This table is to ensure manufacturers are referenced consistently"""
+    manufacturer = models.CharField(max_length=50, null=False)
 
     def __str__(self):
         return self.manufacturer
 
 
 class EquipmentModel(models.Model):
-    manufacturer = models.ForeignKey(EquipmentManufacturer, on_delete=models.PROTECT)
-    model = models.CharField(max_length=100)
-    maintenance_period = models.ForeignKey(Period, help_text="maintenance period.")
-    calibration_period = models.ForeignKey(Period, on_delete=models.PROTECT, help_text="Calibration period.")
+    manufacturer = models.ForeignKey(EquipmentManufacturer, null=False, on_delete=models.PROTECT)
+    model_sku = models.CharField(max_length=100, null=False)
+    maintenance_period = models.ForeignKey(Period, on_delete=models.PROTECT, related_name='maintenance', help_text='Maintenance period.')
+    calibration_period = models.ForeignKey(Period, on_delete=models.PROTECT, related_name='calibration', help_text='Calibration period.')
+    type = models.ForeignKey(EquipmentType, null=False, on_delete=models.PROTECT)
     description = models.CharField(max_length=100)
-    approval_reference = models.CharField(max_length=50)
+    documents = models.FileField(upload_to='mfg_documents/')
+    approval_ref = models.CharField(max_length=50)
+    date = models.DateField(default=date.today, null=False)
+    by = models.CharField(max_length=50, null=False)
 
     def __str__(self):
-        return self.model
+        return f"{self.manufacturer} - {self.model_sku}"
 
 
-class EquipmentInstance(models.Model):
+class Instance(models.Model):
     instance_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    model = models.ForeignKey(EquipmentModel, on_delete=models.CASCADE)
-    internal_id = models.CharField(max_length=50, unique=True)
+    eqp_model = models.ForeignKey(EquipmentModel, on_delete=models.CASCADE)
+    internal_id = models.CharField(max_length=50, unique=True, null=False)
     serial_number = models.CharField(max_length=50)
-    first_use_date = models.DateField("date", default=date.today)
-    calibration_due = models.DateField(help_text="Calibration due date.")
+    entry_date = models.DateField(default=date.today, null=False)
+    by = models.CharField(max_length=50, null=False)
 
     class Meta:
         ordering = ['internal_id']
@@ -46,42 +67,44 @@ class EquipmentInstance(models.Model):
 
 
 class RecordType(models.Model):
-    record_type = models.CharField(max_length=60)
+    """This table is for controlling the types of records for referential integrity"""
+    type = models.CharField(max_length=60, null=False)
+
+    def __str__(self):
+        return self.type
+
+
+class RecordDetail(models.Model):
+    """This table ties the RecordTypes to specific RecordDetails. For Example a calibration record type would be
+    connected to a calibration due record detail. This way the record log would display
+    Calibration / Calibration Due or Calibration / Calibration Performed or Maintenance / Maintenance Due
+    """
+    record_type = models.ForeignKey(RecordType, null=False, on_delete=models.CASCADE)
+    detail = models.CharField(max_length=100, null=False)
 
     def __str__(self):
         return self.record_type
 
 
-class EquipmentRecord(models.Model):
+class RecordLog(models.Model):
+    """This table documents all the records associated with a specific equipment instance. For example a record
+    referencing a verification reference would tie the equipment instance to the record.
+    """
     record_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    equipment_instance = models.ForeignKey(EquipmentInstance, on_delete=models.CASCADE)
-    record_type = models.ForeignKey(RecordType, on_delete=models.CASCADE)
+    instance = models.ForeignKey(Instance, null=False, on_delete=models.CASCADE)
+    record_detail = models.ForeignKey(RecordDetail, null=False, on_delete=models.CASCADE)
     details = models.CharField(max_length=400)
-    date = models.DateField("date", default=date.today)
+    detail_date = models.DateField(default=date.today, help_text='Use this field to enter dates associated with the '
+                                                                 'detail. (e.g., calibration due date, maintenance '
+                                                                 'due date etc.)')
+    documents = models.FileField(upload_to='eqp_records/% Y/% m/% d/')
+    references = models.CharField(max_length=100)
+    entry_date = models.DateField(default=date.today, null=False, help_text='Use this field to record the date of '
+                                                                            'this record.')
+    by = models.CharField(max_length=50, null=False)
 
     class Meta:
-        ordering = ['equipment_instance', 'date']
+        ordering = ['instance', 'entry_date']
 
     def __str__(self):
-        return f"{self.equipment_instance} {self.record_uuid}"
-
-
-class StatusType(models.Model):
-    status_type = models.CharField(max_length=50)
-
-    # def __str__(self):
-    #     return self.status_type
-
-
-class EquipmentStatus(models.Model):
-    status_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    status_type = models.ForeignKey(StatusType, on_delete=models.CASCADE)
-    equipment_instance = models.ForeignKey(EquipmentInstance, on_delete=models.CASCADE)
-    details = models.CharField(max_length=400)
-    date = models.DateField("date", default=date.today)
-
-    class Meta:
-        ordering = ['equipment_instance', 'date']
-
-    def __str__(self):
-        return self.equipment_instance
+        return f"{self.instance} / {self.record_uuid}"
