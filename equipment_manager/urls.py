fcd8ae0aa9994e7f48252448df1f643c1b53e3b3
cris <cgoebner@gmail.com>
2024-11-30 16:42:38 -0800

    Added all of the placeholder templates for the sidebar nav

--- equipment_manager/urls.py
+++ equipment_manager/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
-from .views import ph_index_view
+from .views import (ph_index_view, ph_add_record_view, ph_add_equipment_view, ph_equipment_view, ph_records_view,
+                    ph_test_view)
 
 urlpatterns = [
     path('', ph_index_view, name='index'),
+]
+
+urlpatterns += [
+    path('records/add-record/', ph_add_record_view, name='add record'),
+    path('equipment/add-equipment/', ph_add_equipment_view, name='add equipment'),
+    path('equipment/', ph_equipment_view, name='equipment'),
+    path('records/', ph_records_view, name='records'),
+    path('test/', ph_test_view, name='test'),
 ]
--- equipment_manager/views.py
+++ equipment_manager/views.py
@@ -3,6 +3,37 @@ from django.shortcuts import render
 # Create your views here.
 def ph_index_view(request):
     context = {
-        'ph_index_text': 'This is a placeholder for the add states page.'
+        'ph_index_text': 'This is a placeholder for the index page.'
     }
     return render(request, 'index.html', context)
+
+
+def ph_add_record_view(request):
+    context = {
+        'ph_add_record_text': 'This is a placeholder for the add record page.'
+    }
+    return render(request, 'add_record.html', context)
+
+def ph_add_equipment_view(request):
+    context = {
+        'ph_add_equipment_text': 'This is a placeholder for the add equipment page.'
+    }
+    return render(request, 'add_equipment.html', context)
+
+def ph_records_view(request):
+    context = {
+        'ph_records_text': 'This is a placeholder for the records page.'
+    }
+    return render(request, 'records.html', context)
+
+def ph_equipment_view(request):
+    context = {
+        'ph_equipment_text': 'This is a placeholder for the equipment page.'
+    }
+    return render(request, 'equipment.html', context)
+
+def ph_test_view(request):
+    context = {
+        'ph_test_text': 'This is a placeholder for the test page.'
+    }
+    return render(request, 'test.html', context)
